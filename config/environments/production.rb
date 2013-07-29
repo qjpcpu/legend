ff58872331ebb61be934ec3c78012a942669bdb1
JasonQu <qjpcpu@gmail.com>
2013-07-29 14:10:16 +0800

    internationalization for devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,7 @@ Legend::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
+  config.i18n.available_locales = ['zh-CN', :en]
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
